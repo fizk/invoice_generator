7ebef3a70e8cdcd8aaa5b811c7bb23ae965a9c8a
Ásgeir Berg Matthíasson <asgeir@asgeirberg.org>
2015-09-23 14:17:34 +0200

    added some error checking and more comments to the code

--- api/views.py
+++ api/views.py
@@ -8,18 +8,20 @@ from rest_framework.response import Response
 from rest_framework.exceptions import APIException, ParseError
 from rest_framework import status, permissions, serializers
 
+from pprint import pprint
+
 class Invoice_generator(APIView):
 	""" Create a new invoice for the invoice generator """
 
 	def get(self, request, format=None):
 
 		#
-		# This makes sure ?id=n is the only valid query
+		# Make sure ?id=n is the only valid query
 		#
 
 		for key in request.GET.keys():
 			if key != "id":
-				return Response("Error: I only accept 'id' as keyword")
+				return ParseError("Error: I only accept 'id' as keyword")
 
 		#
 		# parse the query
@@ -37,13 +39,32 @@ class Invoice_generator(APIView):
 		except:
 			raise ParseError('Could not convert id numbers to integers.')
 
+		#
+		# Serialise
+		# 
+
 		invoice_objs = Invoice.objects.filter(pk__in=ids)
 		serializer = InvoiceSerializer(invoice_objs, many=True)
 
 		return Response(serializer.data)
 
 	def post(self, request, format=None):
+
 		serializer = InvoiceSerializer(data=request.data)
+
+		#
+		# Make sure the POST doesn't do any naughty stuff. Having more than five items and messing with 'closed' and 'success' is not allowed
+		#
+		
+		if (len(serializer.initial_data['items']) > 5):
+			raise ParseError("Error: 5 items is the maximum")
+
+		if 'closed' in serializer.initial_data or 'success' in serializer.initial_data:
+			raise ParseError("Error: JSON object contains an invalid field")
+
+		#
+		# Check for validity and save to the database
+		#
 
 		if serializer.is_valid():
 			serializer.save()
